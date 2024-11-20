12b96384144b38aa216840965f8b719676cf3ce8
vialias <intusr74@proton.me>
2024-11-20 12:00:38 +0100

    [fish] yazi can now open with 'y' and change the working directory

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     export EDITOR=vim
+    function y
+    	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    	yazi $argv --cwd-file="$tmp"
+    	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+    		builtin cd -- "$cwd"
+    	end
+    	rm -f -- "$tmp"
+    end
 end

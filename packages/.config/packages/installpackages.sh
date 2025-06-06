fdfc9e7d33c571ca23b8f20672c763acbbb44430
vialias <vialias@proton.me>
2025-06-06 10:46:59 +0200

    [packages] skripte permissions auf owner geändert.

--- packages/.config/packages/installpackages.sh
+++ packages/.config/packages/installpackages.sh
@@ -1,6 +1,8 @@
+#!/bin/zsh
+
 sudo pacman -S --needed - < pacmanpackages.txt
 sudo yay -S --needed - < aurpackages.txt
-for pkg in (cat flatpackpackages.txt)
+for pkg in $(cat flatpackpackages.txt)
     flatpak install -y $pkg
 end
 

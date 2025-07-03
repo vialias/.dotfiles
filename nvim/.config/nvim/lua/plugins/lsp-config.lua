140527e2280950bdc9ebbcb8b3cfc014e3ea0869
vialias <vialias@proton.me>
2025-07-04 01:03:07 +0200

    [nvim] lsp für python hinzugefügt

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "ruff"}
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.ruff.setup({})
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+		end
+	},
+
+}

c1e5723002528d65b1aed256e66c8bfebd727db4
vialias <vialias@proton.me>
2026-01-19 12:53:13 +0100

    [nvim] blink.cmp hinzugefügt, jedoch muss es noch gefinetuend werden.

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,31 +1,35 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "ruff", "pylsp", "bashls", "texlab"}
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-				border = "rounded"
-			})
-			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-				border = "rounded"
-			})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', 'K', vim.diagnostic.open_float, {}) -- fehler und warnings darstellen
-		end
-	},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "saghen/blink.cmp",
+    },
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local lspconfig = require("lspconfig")
 
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd", "ruff", "pylsp", "bashls", "texlab" },
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({ capabilities = capabilities })
+          end,
+        },
+      })
+
+      -- Global handlers and keymaps
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded"
+      })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded"
+      })
+
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "LSP Definition" })
+      vim.keymap.set('n', 'K', vim.diagnostic.open_float, { desc = "LSP Diagnostics" })
+    end
+  },
 }
